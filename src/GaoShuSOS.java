c8ba2bc16dab20d0798b33fcae3cfd058aec49f2
lzo <lzo@ll.com>
2022-07-15 18:23:09 +0800

    提交第三次

--- src/GaoShuSOS.java
+++ src/GaoShuSOS.java
@@ -5,6 +5,7 @@ public class GaoShuSOS {
             System.out.println("高数及格");
             System.out.println("高数及格");
             System.out.println("大物及格");
+            System.out.println("大英及格");
         }
     }
 
