7505a80efba8e0431913e3622d72bdb5c712f87f
lzo <lzo@ll.com>
2022-07-15 18:34:44 +0800

    热修的第一次提交

--- src/GaoShuSOS.java
+++ src/GaoShuSOS.java
@@ -6,6 +6,7 @@ public class GaoShuSOS {
             System.out.println("高数及格");
             System.out.println("大物及格");
             System.out.println("大英及格");
+            System.out.println("全部及格");
         }
     }
 
