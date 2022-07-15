2c159539c4b80576e95f12072f223d6cff66dd26
lzo <lzo@ll.com>
2022-07-15 18:16:58 +0800

    提交第一次

---
+++ src/GaoShuSOS.java
@@ -0,0 +1,10 @@
+public class GaoShuSOS {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 114514; i++) {
+            System.out.println("高数及格");
+            System.out.println("高数及格");
+        }
+    }
+
+}
